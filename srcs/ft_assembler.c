a20d4c3e2da7061118e588513a44a7d35338a9ff
afaddoul <faddoulachraf39@gmail.com>
2020-02-27 19:56:22 +0100

    debbuging mode removed

--- srcs/ft_assembler.c
+++ srcs/ft_assembler.c
@@ -6,7 +6,7 @@
 /*   By: ada <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 02:20:22 by ada               #+#    #+#             */
-/*   Updated: 2020/02/27 19:12:43 by ada              ###   ########.fr       */
+/*   Updated: 2020/02/27 19:14:17 by ada              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,5 @@ void 		ft_assembler(t_env *env)
 			ft_memdel((void**)&env);
 			ft_error();
 		}
-	printf("\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n");
-	while (env->lines->head)
-	{
-		printf("|%s|", ((t_instru*)(env->lines->head->content))->buff);
-		env->lines->head = env->lines->head->next;
-	}
 	//	ft_bend_analys(env);
 }
