dea1ec4e59e904264b9afea2bc4b73c081c5c609
afaddoul <faddoulachraf39@gmail.com>
2020-02-28 16:58:14 +0100

    uncompelte wpork

--- srcs/ft_label_parser.c
+++ srcs/ft_label_parser.c
@@ -6,20 +6,30 @@
 /*   By: ada <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 15:43:20 by ada               #+#    #+#             */
-/*   Updated: 2020/02/28 16:01:28 by ada              ###   ########.fr       */
+/*   Updated: 2020/02/28 16:57:51 by ada              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static int	ft_islabel(char *str)
+static int	ft_islabel(char c)
 {
-	int 	i;
-
-	if ((str[i] >= 97 && str[i] <= 122) || (str[i] >= 48 && str[i] <= 57)
-			|| str[i] == '_')
+	if ((c >= 97 && c <= 122) || (c >= 48 && c <= 57)
+			|| c == '_')
 		return (1);
 	return (0);
 }
 
+t_env 			*ft_label_parser(t_env *env)
+{
+	t_element	*elm;
+	char 		*ptr;
+	int 		i;
 
+	elm = env->lines->head;
+	while (elm)
+	{
+		;
+		elm = elm->next;
+	}
+}
