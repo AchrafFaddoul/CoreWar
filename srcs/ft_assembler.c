270698cb826116ac6204275eab944f1fa18897fc
afaddoul <faddoulachraf39@gmail.com>
2020-03-12 00:20:51 +0100

    ft_pc_counter normed

--- srcs/ft_assembler.c
+++ srcs/ft_assembler.c
@@ -6,15 +6,15 @@
 /*   By: ada <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 02:20:22 by ada               #+#    #+#             */
-/*   Updated: 2020/03/11 18:42:21 by ada              ###   ########.fr       */
+/*   Updated: 2020/03/12 00:00:09 by ada              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-void 				ft_lines_del(void *content)
+void				ft_lines_del(void *content)
 {
-	t_instru 		*instru;
+	t_instru		*instru;
 	t_symbol_tab	*sym_tab;
 
 	instru = (t_instru*)content;
@@ -38,7 +38,7 @@ void 				ft_lines_del(void *content)
 	ft_memdel((void**)&instru);
 }
 
-void 				ft_label_del(void *content)
+void				ft_label_del(void *content)
 {
 	t_label			*lbl;
 
@@ -48,7 +48,7 @@ void 				ft_label_del(void *content)
 	ft_memdel((void**)&lbl);
 }
 
-void 				ft_env_destroy(t_env *env)
+void				ft_env_destroy(t_env *env)
 {
 	if (env->vect)
 		ft_strdel((char**)&(env->vect));
@@ -68,14 +68,14 @@ void 				ft_env_destroy(t_env *env)
 		ft_memdel((void**)&(env));
 }
 
-void 				ft_assembler(t_env *env)
+void				ft_assembler(t_env *env)
 {
 	if (!ft_frontend_analys(env))
 	{
 		ft_env_destroy(env);
 		ft_error();
 	}
-	if(!(ft_backend_analys(env)))
+	if (!(ft_backend_analys(env)))
 	{
 		ft_env_destroy(env);
 		ft_error();
