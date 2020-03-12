d38e44961bd0c9d96f1f18de554c570531446034
afaddoul <faddoulachraf39@gmail.com>
2020-03-12 17:08:00 +0100

    adding mini_unitest scripts and folders

---
+++ asm_script.sh
@@ -0,0 +1,399 @@
+#!/bin/sh
+echo "******************2014 CHAMPS******************"
+cd /Users/ada/Desktop/dyali
+echo "gen my cors"
+./asm examples/Kappa.s
+./asm examples/Machine-gun.s
+./asm examples/MarineKing.s
+./asm examples/Misaka_Mikoto.s
+./asm examples/Rainbow_dash.s
+./asm examples/Varimathras.s
+./asm examples/Wall.s
+./asm examples/_.s
+./asm examples/_honeybadger.s
+./asm examples/big_feet.s
+./asm examples/doge.s
+./asm examples/dubo.s
+./asm examples/gedeon.s
+./asm examples/justin_bee.s
+./asm examples/live.s
+./asm examples/loose.s
+./asm examples/mandragore.s
+./asm examples/meowluigi.s
+./asm examples/salamahenagalabadoun.s
+./asm examples/sam_2.0.s
+./asm examples/skynet.s
+./asm examples/terminator.s
+./asm examples/ultimate-surrender.s
+./asm examples/youforkmytralala.s
+./asm examples/Asombra.s
+./asm examples/THUNDER.s
+./asm examples/casimir.s
+./asm examples/champ.s
+./asm examples/corelol.s
+./asm examples/darksasuke.s
+./asm examples/jinx.s
+./asm examples/littlepuppy.s
+./asm examples/Douceur_power.s
+./asm examples/Explosive_Kitty.s
+./asm examples/Kittystrophic.s
+./asm examples/gateau.s
+./asm examples/mise_a_jour_windows95.s
+./asm examples/overwatch.s
+./asm examples/run_Kitty_RUN.s
+./asm examples/ultima.s
+echo "NEW COMMERS"
+./asm examples/Bazou.s
+./asm examples/Code_eater.s
+./asm examples/Eraser.s
+./asm examples/ForgottenOne.s
+./asm examples/Hidden.s
+./asm examples/Horace.s
+./asm examples/Izi_Miaou.s
+./asm examples/Kitty_Miaou.s
+./asm examples/NoIdea.s
+./asm examples/O-Maley_Miaou.s
+./asm examples/Stargate0_1.s
+./asm examples/TheHarvester.s
+./asm examples/echo.s
+./asm examples/fast.s
+./asm examples/kamelkaze.s
+./asm examples/katchup.s
+./asm examples/lapsang.s
+./asm examples/le_pet_de_nonne.s
+./asm examples/lou_forktologue.s
+./asm examples/master_of_puppets.s
+./asm examples/pai_mu_tan.s
+./asm examples/sencha.s
+./asm examples/tchupka.s
+./asm examples/wave.s
+echo "creates my diffs"
+xxd examples/Kappa.cor > diff_2x
+xxd examples/Machine-gun.cor > diff_3x
+xxd examples/MarineKing.cor > diff_4x
+xxd examples/Misaka_Mikoto.cor > diff_5x
+xxd examples/Rainbow_dash.cor > diff_6x
+xxd examples/Varimathras.cor > diff_7x
+xxd examples/Wall.cor > diff_8x
+xxd examples/_.cor > diff_9x
+xxd examples/_honeybadger.cor > diff_10x
+xxd examples/big_feet.cor > diff_11x
+xxd examples/doge.cor > diff_12x
+xxd examples/dubo.cor > diff_13x
+xxd examples/gedeon.cor > diff_14x
+xxd examples/justin_bee.cor > diff_15x
+xxd examples/live.cor > diff_16x
+xxd examples/loose.cor > diff_17x
+xxd examples/mandragore.cor > diff_18x
+xxd examples/meowluigi.cor > diff_19x
+xxd examples/salamahenagalabadoun.cor > diff_20x
+xxd examples/sam_2.0.cor > diff_21x
+xxd examples/skynet.cor > diff_22x
+xxd examples/terminator.cor > diff_23x
+xxd examples/ultimate-surrender.cor > diff_24x
+xxd examples/youforkmytralala.cor > diff_25x
+xxd examples/Asombra.cor > diff_26x
+xxd examples/THUNDER.cor > diff_27x
+xxd examples/casimir.cor > diff_28x
+xxd examples/champ.cor > diff_29x
+xxd examples/corelol.cor > diff_30x
+xxd examples/darksasuke.cor > diff_31x
+xxd examples/jinx.cor > diff_32x
+xxd examples/littlepuppy.cor > diff_33x
+xxd examples/Douceur_power.s > diff_34x
+xxd examples/Explosive_Kitty.s  > diff_35x
+xxd examples/Kittystrophic.s > diff_36x
+xxd examples/gateau.s > diff_37x
+xxd examples/mise_a_jour_windows95.s > diff_38x
+xxd examples/overwatch.s > diff_39x
+xxd examples/run_Kitty_RUN.s > diff_40x
+xxd examples/ultima.s > diff_41x
+echo "NEW COMMERS"
+xxd examples/Bazou.s > diff_42x
+xxd examples/Code_eater.s > diff_43x
+xxd examples/Eraser.s > diff_44x
+xxd examples/ForgottenOne.s > diff_45x
+xxd examples/Hidden.s > diff_46x
+xxd examples/Horace.s > diff_47x
+xxd examples/Izi_Miaou.s > diff_48x
+xxd examples/Kitty_Miaou.s > diff_49x
+xxd examples/NoIdea.s > diff_50x
+xxd examples/O-Maley_Miaou.s > diff_51x
+xxd examples/Stargate0_1.s > diff_52x
+xxd examples/TheHarvester.s > diff_53x
+xxd examples/echo.s > diff_54x
+xxd examples/fast.s > diff_55x
+xxd examples/kamelkaze.s > diff_56x
+xxd examples/katchup.s > diff_57x
+xxd examples/lapsang.s > diff_58x
+xxd examples/le_pet_de_nonne.s > diff_59x
+xxd examples/lou_forktologue.s > diff_60x
+xxd examples/master_of_puppets.s > diff_61x
+xxd examples/pai_mu_tan.s > diff_62x
+xxd examples/sencha.s > diff_63x
+xxd examples/tchupka.s  > diff_64x
+xxd examples/wave.s > diff_65x
+echo "gen there cors"
+cd /Users/ada/Desktop/dyalhom
+./asm examples/Kappa.s
+./asm examples/Machine-gun.s
+./asm examples/MarineKing.s
+./asm examples/Misaka_Mikoto.s
+./asm examples/Rainbow_dash.s
+./asm examples/Varimathras.s
+./asm examples/Wall.s
+./asm examples/_.s
+./asm examples/_honeybadger.s
+./asm examples/big_feet.s
+./asm examples/doge.s
+./asm examples/dubo.s
+./asm examples/gedeon.s
+./asm examples/justin_bee.s
+./asm examples/live.s
+./asm examples/loose.s
+./asm examples/mandragore.s
+./asm examples/meowluigi.s
+./asm examples/salamahenagalabadoun.s
+./asm examples/sam_2.0.s
+./asm examples/skynet.s
+./asm examples/terminator.s
+./asm examples/ultimate-surrender.s
+./asm examples/youforkmytralala.s
+./asm examples/Asombra.s
+./asm examples/THUNDER.s
+./asm examples/casimir.s
+./asm examples/champ.s
+./asm examples/corelol.s
+./asm examples/darksasuke.s
+./asm examples/jinx.s
+./asm examples/littlepuppy.s
+./asm examples/Douceur_power.s
+./asm examples/Explosive_Kitty.s
+./asm examples/Kittystrophic.s
+./asm examples/gateau.s
+./asm examples/mise_a_jour_windows95.s
+./asm examples/overwatch.s
+./asm examples/run_Kitty_RUN.s
+./asm examples/ultima.s
+echo "NEW COMMERS"
+./asm examples/Bazou.s
+./asm examples/Code_eater.s
+./asm examples/Eraser.s
+./asm examples/ForgottenOne.s
+./asm examples/Hidden.s
+./asm examples/Horace.s
+./asm examples/Izi_Miaou.s
+./asm examples/Kitty_Miaou.s
+./asm examples/NoIdea.s
+./asm examples/O-Maley_Miaou.s
+./asm examples/Stargate0_1.s
+./asm examples/TheHarvester.s
+./asm examples/echo.s
+./asm examples/fast.s
+./asm examples/kamelkaze.s
+./asm examples/katchup.s
+./asm examples/lapsang.s
+./asm examples/le_pet_de_nonne.s
+./asm examples/lou_forktologue.s
+./asm examples/master_of_puppets.s
+./asm examples/pai_mu_tan.s
+./asm examples/sencha.s
+./asm examples/tchupka.s
+./asm examples/wave.s
+echo "creates there diffs"
+xxd examples/Kappa.cor > diff_2y
+xxd examples/Machine-gun.cor > diff_3y
+xxd examples/MarineKing.cor > diff_4y
+xxd examples/Misaka_Mikoto.cor > diff_5y
+xxd examples/Rainbow_dash.cor > diff_6y
+xxd examples/Varimathras.cor > diff_7y
+xxd examples/Wall.cor > diff_8y
+xxd examples/_.cor > diff_9y
+xxd examples/_honeybadger.cor > diff_10y
+xxd examples/big_feet.cor > diff_11y
+xxd examples/doge.cor > diff_12y
+xxd examples/dubo.cor > diff_13y
+xxd examples/gedeon.cor > diff_14y
+xxd examples/justin_bee.cor > diff_15y
+xxd examples/live.cor > diff_16y
+xxd examples/loose.cor > diff_17y
+xxd examples/mandragore.cor > diff_18y
+xxd examples/meowluigi.cor > diff_19y
+xxd examples/salamahenagalabadoun.cor > diff_20y
+xxd examples/sam_2.0.cor > diff_21y
+xxd examples/skynet.cor > diff_22y
+xxd examples/terminator.cor > diff_23y
+xxd examples/ultimate-surrender.cor > diff_24y
+xxd examples/youforkmytralala.cor > diff_25y
+xxd examples/Asombra.cor > diff_26y
+xxd examples/THUNDER.cor > diff_27y
+xxd examples/casimir.cor > diff_28y
+xxd examples/champ.cor > diff_29y
+xxd examples/corelol.cor > diff_30y
+xxd examples/darksasuke.cor > diff_31y
+xxd examples/jinx.cor > diff_32y
+xxd examples/littlepuppy.cor > diff_33y
+xxd examples/Douceur_power.s > diff_34y
+xxd examples/Explosive_Kitty.s  > diff_35y
+xxd examples/Kittystrophic.s > diff_36y
+xxd examples/gateau.s > diff_37y
+xxd examples/mise_a_jour_windows95.s > diff_38y
+xxd examples/overwatch.s > diff_39y
+xxd examples/run_Kitty_RUN.s > diff_40y
+xxd examples/ultima.s > diff_41y
+xxd examples/Bazou.s > diff_42y
+xxd examples/Code_eater.s > diff_43y
+xxd examples/Eraser.s > diff_44y
+xxd examples/ForgottenOne.s > diff_45y
+xxd examples/Hidden.s > diff_46y
+xxd examples/Horace.s > diff_47y
+xxd examples/Izi_Miaou.s > diff_48y
+xxd examples/Kitty_Miaou.s > diff_49y
+xxd examples/NoIdea.s > diff_50y
+xxd examples/O-Maley_Miaou.s > diff_51y
+xxd examples/Stargate0_1.s > diff_52y
+xxd examples/TheHarvester.s > diff_53y
+xxd examples/echo.s > diff_54y
+xxd examples/fast.s > diff_55y
+xxd examples/kamelkaze.s > diff_56y
+xxd examples/katchup.s > diff_57y
+xxd examples/lapsang.s > diff_58y
+xxd examples/le_pet_de_nonne.s > diff_59y
+xxd examples/lou_forktologue.s > diff_60y
+xxd examples/master_of_puppets.s > diff_61y
+xxd examples/pai_mu_tan.s > diff_62y
+xxd examples/sencha.s > diff_63y
+xxd examples/tchupka.s  > diff_64y
+xxd examples/wave.s > diff_65y
+echo "NEW COMMERS"
+cp diff* /Users/ada/Desktop/dyali
+cd /Users/ada/Desktop/dyali
+diff diff_2x diff_2y
+diff diff_3x diff_3y
+diff diff_4x diff_4y
+diff diff_5x diff_5y
+diff diff_6x diff_6y
+diff diff_7x diff_7y
+diff diff_8x diff_8y
+diff diff_9x diff_9y
+diff diff_10x diff_10y
+diff diff_11x diff_11y
+diff diff_12x diff_12y
+diff diff_13x diff_13y
+diff diff_14x diff_14y
+diff diff_15x diff_15y
+diff diff_16x diff_16y
+diff diff_17x diff_17y
+diff diff_18x diff_18y
+diff diff_19x diff_19y
+diff diff_20x diff_20y
+diff diff_21x diff_21y
+diff diff_22x diff_22y
+diff diff_23x diff_23y
+diff diff_24x diff_24y
+diff diff_25x diff_25y
+diff diff_26x diff_26y
+diff diff_27x diff_27y
+diff diff_28x diff_28y
+diff diff_29x diff_29y
+diff diff_30x diff_30y
+diff diff_31x diff_31y
+diff diff_32x diff_32y
+diff diff_33x diff_33y
+diff diff_34x diff_34y
+diff diff_35x diff_35y
+diff diff_36x diff_36y
+diff diff_37x diff_37y
+diff diff_38x diff_38y
+diff diff_39x diff_39y
+diff diff_40x diff_40y
+diff diff_41x diff_41y
+diff diff_42x diff_42y
+diff diff_43x diff_43y
+diff diff_44x diff_44y
+diff diff_45x diff_45y
+diff diff_46x diff_46y
+diff diff_47x diff_47y
+diff diff_48x diff_48y
+diff diff_49x diff_49y
+diff diff_50x diff_50y
+diff diff_51x diff_51y
+diff diff_52x diff_52y
+diff diff_53x diff_53y
+diff diff_54x diff_54y
+diff diff_55x diff_55y
+diff diff_56x diff_56y
+diff diff_57x diff_57y
+diff diff_58x diff_58y
+diff diff_59x diff_59y
+diff diff_60x diff_60y
+diff diff_61x diff_61y
+echo "NEW COMMERS"
+echo "*******2014-CHAMPS-TEST********"
+cd /Users/ada/Desktop/dyalhom
+rm diff*
+cd /Users/ada/Desktop/dyali
+rm diff*
+echo "*******TEST******2014***********DONE*************"
+echo ""
+echo ""
+echo ""
+echo "******************EXEMPLES CHAMPS******************"
+#!/bin/sh
+cd /Users/ada/Desktop/dyali
+echo "gen my cors"
+./asm examples/bee_gees.s
+./asm examples/bigzork.s
+./asm examples/fluttershy.s
+./asm examples/helltrain.s
+./asm examples/turtle.s
+./asm examples/zork.s
+./asm examples/42.s
+echo "creates my diffs"
+xxd examples/bee_gees.cor > diff_1x
+xxd examples/bigzork.cor > diff_2x
+xxd examples/fluttershy.cor > diff_3x
+xxd examples/helltrain.cor > diff_4x
+xxd examples/turtle.cor > diff_5x
+xxd examples/zork.cor > diff_6x
+xxd examples/42.cor > diff_7x
+echo "gen there cors"
+cd /Users/ada/Desktop/dyalhom
+./asm examples/bee_gees.s
+./asm examples/bigzork.s
+./asm examples/fluttershy.s
+./asm examples/helltrain.s
+./asm examples/turtle.s
+./asm examples/zork.s
+./asm examples/42.s
+echo "creates there diffs"
+echo "diff *1*"
+xxd examples/bee_gees.cor > diff_1y
+echo "diff *2*"
+xxd examples/bigzork.cor > diff_2y
+echo "diff *3*"
+xxd examples/fluttershy.cor > diff_3y
+echo "diff *4*"
+xxd examples/helltrain.cor > diff_4y
+echo "diff *5*"
+xxd examples/turtle.cor > diff_5y
+echo "diff *6*"
+xxd examples/zork.cor > diff_6y
+echo "diff *7*"
+xxd examples/42.cor > diff_7y
+cp diff* /Users/ada/Desktop/dyali
+cd /Users/ada/Desktop/dyali
+diff diff_1x diff_1y
+diff diff_2x diff_2y
+diff diff_3x diff_3y
+diff diff_4x diff_4y
+diff diff_5x diff_5y
+diff diff_6x diff_6y
+diff diff_7x diff_7y
+echo "DONE PASSED"
+echo "********EXEMPLES-TEST-FINISHED****************"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "********PERFECTO****************"
