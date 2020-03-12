6f52e9bcac4456343768a1c9695f483b97d57575
afaddoul <faddoulachraf39@gmail.com>
2020-03-12 17:03:26 +0100

    norme done

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: afaddoul <afaddoul@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 09:41:11 by afaddoul          #+#    #+#             */
-/*   Updated: 2020/03/12 16:49:10 by ada              ###   ########.fr       */
+/*   Updated: 2020/03/12 17:00:51 by ada              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -211,7 +211,7 @@ int					ft_arg_filler(t_element *elm, char *arg);
 t_env				*ft_get_label(t_env *env, char *ptr, int *i);
 int					ft_tokenize_arg(t_ptrs *ptrs, t_element *elm, char *str,
 		int *i);
-int					ft_get_cmd(t_env *env, t_element *elm, char *ptr,
+t_element			*ft_get_cmd(t_env *env, t_element *elm, char *ptr,
 		int i);
 t_env				*ft_parsing_done(t_env *env, t_element *elm);
 int					get_first_name_line(char *buff, const char *ptr, int *i,
